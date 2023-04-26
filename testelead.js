b5f81be7ab35f398aa359aed63f1c97df064cc20
Gabrielle Ciriaco <122090507+gabrielleciriaco@users.noreply.github.com>
2023-04-26 19:58:38 +0100

    Create testelead.js

---
+++ testelead.js
@@ -0,0 +1,13 @@
+import { LightningElement, api } from "lwc";
+
+export default class testelead extends LightningElement {
+  @api ide;
+  @api nome;
+  @api empresa;
+  @api status;
+  @api sobnome;
+  @api email;
+  @api idade;
+  @api classif;  
+  
+}
